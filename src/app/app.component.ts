83efd9f46ac56f9f0a096f83a50c174da06e0acb
Chonlatas Tasanapreechachai <chonlatas.t@gmail.com>
2019-06-04 16:35:06 +0700

    test input ang event angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-app';
+
+  parentName: string;
+
+  setName(parentName) {
+    this.parentName = parentName;
+  }
 }
